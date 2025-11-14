b93bdfc4427e90ec5025a16b5f75d6ab46115784
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-11-14 09:18:48 -0500

    Able to semi parse local html race file

--- server/controllers/Championship.js
+++ server/controllers/Championship.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const { JSDOM } = require('jsdom');
 const models = require('../models');
 
 const { Championship } = models;
@@ -15,12 +17,26 @@ const championshipsPage = async (req, res) => {
   }
 };
 
+function extractData(table) {
+  return Array.from(table).map((row) => {
+    const cells = row.cells;
+    return cells.map((cell) => cell.textContent.trim()).join(",");
+  });
+}
+
 const addRace = async (req, res) => {
   try {
     const query = { owner: req.session.account._id, name: req.body.name };
-    const docs = await Championship.find(query).select('name').lean().exec();
+    const championshipToAddTo = await Championship.find(query).select('name').lean().exec();
 
-    console.log(docs);
+    const htmlContent = fs.readFileSync(`${__dirname}/race.html`, 'utf8');
+    const domOfRace = new JSDOM(htmlContent);
+    const tables = domOfRace.window.document.querySelectorAll('table');
+    // tables.forEach((table) => {
+    //   console.log(table.outerHTML);
+    // });
+    console.log(tables[4].outerHTML);
+
     // return res.render('championships', { championships: docs });
   } catch (err) {
     console.log(err);
@@ -47,7 +63,9 @@ const makeChampionship = async (req, res) => {
     if (err.code === 11000) {
       return res.status(400).json({ error: 'Championship already exists!' });
     }
-    return res.status(500).json({ error: 'An error occured making championship!' });
+    return res
+      .status(500)
+      .json({ error: 'An error occured making championship!' });
   }
 };
 
